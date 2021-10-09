25995a8421be612830283b83283efb75aa6a79a1
Balaji PJ <pjbalaji25@gmail.com>
2021-10-09 19:32:17 +0530

    mongodb import bug

--- go.mod
+++ go.mod
@@ -1,5 +1,8 @@
-module Backend
+module Instagram
 
-go 1.13
+go 1.17
 
-require github.com/gorilla/mux v1.8.0
+require (
+	github.com/gorilla/mux v1.8.0 // indirect
+	go.mongodb.org/mongo-driver v1.7.3 // indirect
+)
--- main.go
+++ main.go
@@ -1,17 +1,22 @@
 package main
 
 import (
+	"Instagram/Controller"
+	"context"
+	"fmt"
 	"log"
 	"net/http"
+
 	"github.com/gorilla/mux"
-	"Backend/Controller"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
 )
 
 func main() {
 	log.Printf("Server started")
 
 	// Routing using mux---------------------------------------------------------
-	
+
 	router := mux.NewRouter()
 	router.HandleFunc("/posts", Controller.CreatePost).Methods("POST")
 	router.HandleFunc("/posts/{postId}", Controller.FetchPost).Methods("GET")
@@ -21,6 +26,23 @@ func main() {
 
 	// --------------------------------------------------------------------------
 
-	log.Fatal(http.ListenAndServe(":8080", router))
-	
-}
+	log.Fatal(http.ListenAndServe(":8079", router))
+
+	clientOptions := options.Client().ApplyURI("mongodb://localhost:27016")
+
+	client, err := mongo.Connect(context.TODO(), clientOptions)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	err = client.Ping(context.TODO(), nil)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println("Connected to MongoDB!")
+
+	}
+
